21b2f577ce250e975853fd344a7edfa6f9ecd74b
Matthew Fitzgerald <mattlol85@gmail.com>
2023-08-15 00:32:37 -0400

    chore(add-formatter): added google formatter to run on build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import com.github.sherter.googlejavaformatgradleplugin.GoogleJavaFormat
+import com.github.sherter.googlejavaformatgradleplugin.VerifyGoogleJavaFormat
+
 plugins {
     java
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
+    id("com.github.sherter.google-java-format") version "0.9"
 }
 
 group = "org.fitznet"
@@ -36,3 +40,28 @@ dependencies {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+googleJavaFormat {
+    toolVersion = "1.7"
+}
+
+tasks.register<GoogleJavaFormat>("format") {
+    source("src/main")
+    source("src/test")
+    include("**/*.java")
+}
+
+tasks.register<VerifyGoogleJavaFormat>("verifyFormatting") {
+    source("src/main")
+    include("**/*.java")
+    ignoreFailures = false
+}
+
+tasks.named("verifyFormatting") {
+    dependsOn("format")
+}
+
+tasks.named("check") {
+dependsOn("verifyFormatting")
+}
+
+apply(plugin = "com.github.sherter.google-java-format")
