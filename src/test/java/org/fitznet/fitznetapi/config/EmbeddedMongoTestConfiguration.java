848d8a746852c37d118c949e53a04d159838746f
Matthew Fitzgerald <mattlol85@gmail.com>
2026-02-23 21:28:00 -0500

    feat(refactor): Update EmbeddedMongoTestConfiguration and application.properties for improved MongoDB compatibility

--- src/test/java/org/fitznet/fitznetapi/config/EmbeddedMongoTestConfiguration.java
+++ src/test/java/org/fitznet/fitznetapi/config/EmbeddedMongoTestConfiguration.java
@@ -1,8 +1,6 @@
 package org.fitznet.fitznetapi.config;
 
-import de.flapdoodle.os.CommonOS;
-import de.flapdoodle.os.ImmutablePlatform;
-import de.flapdoodle.os.Platform;
+import de.flapdoodle.embed.mongo.distribution.Version;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
@@ -10,23 +8,9 @@ import org.springframework.context.annotation.Bean;
 public class EmbeddedMongoTestConfiguration {
 
     @Bean
-    public Platform platform() {
-        String osName = System.getProperty("os.name").toLowerCase();
-        String osArch = System.getProperty("os.arch").toLowerCase();
-
-        // M1 Mac (ARM64)
-        if (osName.contains("mac") && osArch.contains("aarch64")) {
-            return ImmutablePlatform.builder()
-                .operatingSystem(CommonOS.OS_X)
-                .architecture(de.flapdoodle.os.CommonArchitecture.ARM_64)
-                .build();
-        }
-
-        // Generic Linux x86_64 (for CI and other platforms)
-        return ImmutablePlatform.builder()
-            .operatingSystem(CommonOS.Linux)
-            .architecture(de.flapdoodle.os.CommonArchitecture.X86_64)
-            .build();
+    public Version.Main version() {
+        // Use MongoDB 4.4 which has better platform support
+        return Version.Main.V4_4;
     }
 }
 
